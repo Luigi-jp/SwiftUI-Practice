a245c36a269cd365097ac7917043d3f7f765e2ee
luigi sato <base.basketball0607@gmail.com>
2022-05-02 18:04:49 +0900

    Initial commit

---
+++ SwiftUI-Practice/SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  SwiftUI-Practice
+//
+//  Created by 佐藤瑠偉史 on 2022/05/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
