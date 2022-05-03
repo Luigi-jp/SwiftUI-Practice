e013700d4316e32c7dfb85b4dadf6d53c28ef145
luigi sato <base.basketball0607@gmail.com>
2022-05-03 10:02:01 +0900

    Creating and Combining Views

---
+++ SwiftUI-Practice/MapView.swift
@@ -0,0 +1,27 @@
+//
+//  MapView.swift
+//  SwiftUI-Practice
+//
+//  Created by 佐藤瑠偉史 on 2022/05/03.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_869),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+            .previewDevice("iPhone 13")
+    }
+}
