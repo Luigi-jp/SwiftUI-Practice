86a142c4c291e164edd08fd7ce14b9d427ff1e9c
luigi sato <base.basketball0607@gmail.com>
2022-05-05 20:49:58 +0900

    Section6 Set Up Navigation Between List and Detail

---
+++ SwiftUI-Practice/Views/LandmarkDetail.swift
@@ -0,0 +1,52 @@
+//
+//  LandmarkDetail.swift
+//  SwiftUI-Practice
+//
+//  Created by 佐藤瑠偉史 on 2022/05/05.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+            .previewDevice("iPhone 13")
+    }
+}
