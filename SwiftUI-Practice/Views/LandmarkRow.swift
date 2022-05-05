d4e1ae184422ad7ce84223753d924a64c226ca07
luigi sato <base.basketball0607@gmail.com>
2022-05-05 20:12:33 +0900

    Section2 Create the Row View

---
+++ SwiftUI-Practice/Views/LandmarkRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkRow.swift
+//  SwiftUI-Practice
+//
+//  Created by 佐藤瑠偉史 on 2022/05/05.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
