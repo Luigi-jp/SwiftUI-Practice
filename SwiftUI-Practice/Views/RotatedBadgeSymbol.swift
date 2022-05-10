a450c543b7058c873140e614f08610c8b1de4af9
luigi sato <base.basketball0607@gmail.com>
2022-05-10 19:20:58 +0900

    Section3 Draw the Badge Symbol

---
+++ SwiftUI-Practice/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftUI-Practice
+//
+//  Created by 佐藤瑠偉史 on 2022/05/10.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
